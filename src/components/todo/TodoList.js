2170a5315b318e783f1bd3e6113c8cfd4073abe1
Lee Delarm <lee.delarm@gmail.com>
2017-04-20 20:16:19 -0500

    Added missing files

---
+++ src/components/todo/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {TodoItem} from './TodoItem';
+
+export const TodoList = (props) => {
+    return (
+        <div className="Todo-List">
+            <ul>
+              {props.todos.map(todo => <TodoItem {...todo} key={todo.id} /> )}
+            </ul>
+          </div>
+    )
+}
